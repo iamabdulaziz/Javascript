29f6f58402ef1ad7b9ce719b46d27f7ba3b283c1
Abdul Aziz <abdul.aziz@hopperbee.com>
2019-12-02 22:10:52 +0530

    function array object

---
+++ obj-this.js
@@ -0,0 +1,16 @@
+// Objects and methods
+
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',
+    birthYear: 1992,
+    family: ['Jane', 'Mark', 'Bob', 'Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(){
+        this.age = 2018 - this.birthYear;
+    }
+};
+
+john.calcAge();
+console.log(john);
